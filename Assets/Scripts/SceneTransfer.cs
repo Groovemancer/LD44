db01068a3dd0f877512876748d4c13fdd6b04275
icedmetal57 <icedmetal57@gmail.com>
2019-04-26 23:20:59 -0500

    Scene Transfer Script

---
+++ Assets/Scripts/SceneTransfer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransfer : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        Debug.Log("Attempting to load scene: " + sceneName);
+        SceneManager.LoadScene(sceneName);
+    }
+}
